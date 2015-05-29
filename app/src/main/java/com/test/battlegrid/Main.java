b37877a25c381b7d0c070cf3e3c156b53516f490
hennign <hennign@onid.oregonstate.edu>
2015-05-29 15:10:38 -0700

    Replaced the settings page with menu buttons. Merged rest of code into Main.java because I couldn't how to make various things communicate from separate classes.

--- app/src/main/java/com/test/battlegrid/Main.java
+++ app/src/main/java/com/test/battlegrid/Main.java
@@ -1,38 +1,62 @@
 package com.test.battlegrid;
 
+
+import android.app.Dialog;
 import android.content.ClipData;
-import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.internal.view.menu.ActionMenuItemView;
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 
 public class Main extends ActionBarActivity {
+
+    private ImageView[] targets = new ImageView[45];
+
+    public boolean is_server = false;
+
+    private ServerSocket serverSocket;
+
+    Handler updateConversationHandler;
+
+    Thread serverThread = null;
+
+    Dialog dialog;
+
+    public static final int SERVER_PORT = 6000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.ball1).setOnLongClickListener(listenClick);
-        findViewById(R.id.ball1).setOnDragListener(listenDrag);
-        findViewById(R.id.target1).setOnLongClickListener(listenClick);
-        findViewById(R.id.target1).setOnDragListener(listenDrag);
+        updateConversationHandler = new Handler();
 
-        GridLayout gridLayout = (GridLayout)findViewById(R.id.grid1);
+
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.grid1);
 
         gridLayout.removeAllViews();
 
@@ -62,9 +86,8 @@ public class Main extends ActionBarActivity {
                 oImageView.setImageResource(R.drawable.target);
             }
 
-            GridLayout.LayoutParams param =new GridLayout.LayoutParams();
-            //param.height = GridLayout.LayoutParams.WRAP_CONTENT;
-            //param.width = GridLayout.LayoutParams.WRAP_CONTENT;
+            GridLayout.LayoutParams param = new GridLayout.LayoutParams();
+
             param.rightMargin = 18;
             param.leftMargin = 15;
             param.topMargin = 5;
@@ -78,10 +101,98 @@ public class Main extends ActionBarActivity {
             oImageView.setOnLongClickListener(listenClick);
             oImageView.setOnDragListener(listenDrag);
 
+            targets[i] = oImageView;
+
             gridLayout.addView(oImageView);
         }
 
     }
+
+    class ServerThread implements Runnable {
+
+        public void run() {
+            Socket socket = null;
+            try {
+                serverSocket = new ServerSocket(SERVER_PORT);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while (!Thread.currentThread().isInterrupted()) {
+
+                try {
+
+                    socket = serverSocket.accept();
+
+                    CommunicationThread commThread = new CommunicationThread(socket);
+                    new Thread(commThread).start();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class CommunicationThread implements Runnable {
+
+        private Socket clientSocket;
+
+        private BufferedReader input;
+
+        public CommunicationThread(Socket clientSocket) {
+
+            this.clientSocket = clientSocket;
+
+            try {
+
+                this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+
+            while (!Thread.currentThread().isInterrupted()) {
+
+                try {
+
+                    String read = input.readLine();
+
+                    updateConversationHandler.post(new updateUIThread(read));
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // only try to close serverSocket if it is open, else crash
+        if (is_server) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    View.OnClickListener connector = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            EditText ip = (EditText) dialog.findViewById(R.id.tv);
+            Log.i("TEST", "Connect button clicked with value " + ip.getText().toString());
+        }
+    };
+
 
 
     @Override
@@ -100,8 +211,46 @@ public class Main extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, settings.class);
-            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.checkBox) {
+
+            if (!is_server) {
+                is_server = true;
+                Log.i("TEST", "Set to server mode: true");
+                Main.this.serverThread = new Thread(new ServerThread());
+                Main.this.serverThread.start();
+                Toast.makeText(getBaseContext(), "Server Mode: On", Toast.LENGTH_SHORT).show();
+
+            } else {
+                is_server = false;
+                Log.i("TEST", "Set to server mode: false");
+                //TODO - figure out how to shutdown server mode (not important)
+                serverThread.interrupt();
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+                Toast.makeText(getBaseContext(), "Server Mode: Off", Toast.LENGTH_SHORT).show();
+            }
+
+            return true;
+        }
+        if (id == R.id.button) {
+            Log.i("BATGRID", "button pressed");
+            dialog = new Dialog(Main.this);
+            dialog.setContentView(R.layout.dialog);
+
+
+            dialog.setTitle("Connect to a Battle Grid Server:");
+            dialog.show();
+
+            Button b = (Button) dialog.findViewById(R.id.server_connect);
+            b.setOnClickListener(connector);
+
             return true;
         }
 
@@ -187,8 +336,6 @@ public class Main extends ActionBarActivity {
 
                     Toast.makeText(getBaseContext(), ids, Toast.LENGTH_SHORT).show();
 
-
-
                     dragged.setImageDrawable(target_draw);
                     target.setImageDrawable(dragged_draw);
 
@@ -199,5 +346,34 @@ public class Main extends ActionBarActivity {
             return true;
         }
     };
+
+    class updateUIThread implements Runnable {
+
+        private String msg;
+
+        public updateUIThread(String str) {
+            this.msg = str;
+        }
+
+        @Override
+        public void run() {
+            Log.i("updateUIThread", msg);
+            //TODO - parse numbers from string and set id1 and id2
+            int id1 = 27;
+            int id2 = 15;
+            String ids = "From "+ id2 + " to " + id1;
+
+            ImageView target = targets[27];
+            ImageView dragged = targets[15];
+
+            Drawable target_draw = target.getDrawable();
+            Drawable dragged_draw = dragged.getDrawable();
+
+            Toast.makeText(getBaseContext(), ids, Toast.LENGTH_SHORT).show();
+
+            dragged.setImageDrawable(target_draw);
+            target.setImageDrawable(dragged_draw);
+        }
+    }
 }
 
